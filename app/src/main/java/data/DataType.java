cfccfddf877455325970efdfeeb58b3d171bbb6c
matburgess <mbur0038@student.monash.edu>
2021-06-20 13:54:21 +1000

    Update file paths to work with distribution files

--- app/src/main/java/data/DataType.java
+++ app/src/main/java/data/DataType.java
@@ -1,13 +1,26 @@
 package data;
 
+import java.io.File;
+import java.net.URISyntaxException;
+
 public enum DataType {
-    USER("src/main/resources/users.dat"),
-    JOB("src/main/resources/jobs.dat");
+    USER("users.txt"),
+    JOB("jobs.txt");
 
-    private String filePath;
+    private final String filePath;
 
-    DataType(String filePath) {
-        this.filePath = filePath;
+    DataType(String fileName) {
+
+        String dir = "";
+
+        try {
+            dir = new File(DataType.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
+        } catch (URISyntaxException ex) {
+            System.out.println("Could not locate data files.");
+        }
+
+        this.filePath = dir + File.separator + fileName;
+
     }
 
     public String getFilePath() {
